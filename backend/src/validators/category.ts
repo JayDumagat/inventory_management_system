ad0b0908f826b86d67b6bc666f199b2650361adf
Jayson Dumagat <jaysondumagat23@gmail.com>
2026-04-15 22:49:10 +0800

    new stuff

--- backend/src/validators/category.ts
+++ backend/src/validators/category.ts
@@ -3,5 +3,9 @@ import { z } from "zod";
 export const categorySchema = z.object({
   name: z.string().min(1),
   description: z.string().optional(),
-  parentId: z.string().uuid().optional().nullable(),
+  parentId: z
+      .string()
+      .nullable()
+      .optional()
+      .transform((val) => (val === "" || val === undefined ? null : val)),
 });
