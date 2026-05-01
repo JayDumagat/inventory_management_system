f011ad5f7a10de2a3a8080f88f2bae98fdd8cc42
Jayson Dumagat <jaysondumagat23@gmail.com>
2026-05-01 14:36:57 +0800

    reset migration

--- backend/src/db/migrations/0004_receipt_logo_and_track_stock.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "tenants" ADD COLUMN "receipt_logo_url" text;
-ALTER TABLE "tenants" ADD COLUMN "receipt_show_logo" boolean DEFAULT false NOT NULL;
-ALTER TABLE "products" ADD COLUMN "track_stock" boolean DEFAULT true NOT NULL;
